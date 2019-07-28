21f35d28016b326bbe37ab92cc0441c6f2d3b464
Johnson Joseph <jhonsonj88@gmail.com>
2019-07-28 22:56:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,16 @@ function distanceTravelledInFeet(start, destination){
     return (start - destination) * 264;
   }
 }
+
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet (start, destination)
+  if (distance < 400) {
+    return 0;
+  } else if (distance > 400 && distance < 2000){
+    return ((distance - 400) * .02);
+  } else if (distance > 2000 && distance < 2500) {
+  return 25;
+  } else if (distance > 2500){
+    return 'cannot travel that far';
+  }
+}
